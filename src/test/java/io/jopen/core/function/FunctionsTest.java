066721c8f4fe06f16326d63a452bc34d5ac94a79
ubuntu-m <m17793873123@163.com>
2019-04-12 20:32:54 +0800

    update json

--- src/test/java/io/jopen/core/function/FunctionsTest.java
+++ src/test/java/io/jopen/core/function/FunctionsTest.java
@@ -4,6 +4,9 @@ import com.google.common.base.Function;
 import com.google.common.base.Functions;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author maxuefeng
  * @see java.util.function.Function
@@ -18,5 +21,19 @@ public class FunctionsTest {
         Object a = identity.apply("a");
 
         System.out.println(a);
+
+        Map<String, Integer> map = new HashMap<String, Integer>() {
+            //构造一个测试用Map集合
+            {
+                put("love", 1);
+                put("miss", 2);
+            }
+        };
+
+        Function<String, Integer> function = Functions.forMap(map);
+
+        Integer result = function.apply("love");
+
+        System.err.println(result);
     }
 }
