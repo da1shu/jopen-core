9dc85bfbfaa0133e2031523a0889c1866567b3eb
ubuntu-emotion <m17793873123@163.com>
2019-04-25 22:23:50 +0800

    update strings

--- src/test/java/io/jopen/core/algorithm/common/RSA1Test.java
+++ src/test/java/io/jopen/core/algorithm/common/RSA1Test.java
@@ -25,17 +25,25 @@ public class RSA1Test {
     }
 
     public static void main(String[] args) {
-        System.err.println("MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCwHsQXdRU25T8MYL7PN6avlD7ao1VSfy3K6GiU+KElMa1hm9kXuA79QPoSEiDs1guJK0mU6WacpVZyyueWvD6wGAZrXJ2RhJzb7xii4Qlx3pBhEP3/dvP56jAmD7qO16FRoNcMZYD5ojhVlDpqtbjmDn2B5fWBjFH/sgmvDQZnKwIDAQAB".equals("MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCLNbmKl9/gLn7Bef/xtUkshC1WyrLZLRpXCcFYR1gQi0isWsZBTicC4efBOkkNG3r+1ue0gvtuU/tjREFGf4Y7HaKHGb5tNCOlMNeNjM5YLRwLFqrUSsQyD4rj4eua1ltearr24R0HilnTvnQm6Z/UY0s21vdOUFQBPY0GNAa+0wIDAQAB"));
+
+        String publicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCdVUemCpFbHk4qQDK3P/FOs5oHZJsNg75Dy6JKqktG1QJgwQ2o5giA+GxGYYQxMhdRzZ4FxDGEQHe2GE/CQjei/orSY8qhzwTM3fDuOh2J4YHZYzRha2swPoD9ZZHqVMw/dBmlsgwVsnwxkvO6so9j7bW4kKIcm+s3qMglJk4zFQIDAQAB";
+        String privateKey = "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAJ1VR6YKkVseTipAMrc/8U6zmgdkmw2DvkPLokqqS0bVAmDBDajmCID4bEZhhDEyF1HNngXEMYRAd7YYT8JCN6L+itJjyqHPBMzd8O46HYnhgdljNGFrazA+gP1lkepUzD90GaWyDBWyfDGS87qyj2PttbiQohyb6zeoyCUmTjMVAgMBAAECgYABZkvP31IxtFBPseiBltEHuTNKk4uJkD12Du6vqVooFCcEQ1NiyEEVHKCh86YgZeitMp4QQRGLsqMd/JNhEAO8rNGOFCR6a+VLXPbE2T4gk4encOZz4ul2/7bM/b43zSKaJLti5JWbRf0KJDjJUAXoXxvKVsfX1sP+W4OR3GARwQJBAPPNoqbmpi3k1p+MamegnWtolsiy9CdfujhNSfK4vQ8oz/DmKsM95SCvAGKbULRrdIzbiHF6m4LoSoezO3kzacUCQQClNDrZMCPc9+OwEEna13WdFADu3Gp4lWFOviZBk48JvhlM9Uqk0OMGd4JPHVbBcly1h3Breqmq3m0gPESYf0kRAkBDcfW3GkOKmq3NAXlGJaam2g4sg2ULp23KuYblgLm8gdvUlrfpvyutuPvwS2suHjPl6615RyPSORS1KZ4gT1rJAkBwKXDW6k8/A/JLJBPwh54rolwrz+xdqBXjz0fL9/ICTFWDIXqnQccRKeyH6ERxnEKDYMwbvLEa+F6gpypvWuPxAkB4dco6o5wF1FGte3XkzFJbPh4eoEfwNiWwk/vgHa0Ggdk2TMAZM3xXcDMThchqRjO9so4OokFAtEw3Z5xMShFw";
+        String str = "{\"code\":1,\"data\":\"数据转换成功\",\"msg\":\"OK\",\"success\":true}";
+
+
+        // 加密
+        String enStr1 = encryptByPublic(str, publicKey);
+
+        System.err.println(enStr1);
+
+        //
+        String source = decryptByPrivate(enStr1, privateKey);
+
+        System.err.println(source);
     }
 
     @Test
     public void testSimpleAPI() {
-
-//        Map<String, Object> map = new HashMap<String, Object>();
-//        map = RSA.init();
-//        System.out.println("公钥："+RSA.getPublicKey(map));
-//        System.out.println("私钥："+RSA.getPrivateKey(map));
-        //由前四行代码获得公、私密钥
 
         String publicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCLNbmKl9/gLn7Bef/xtUkshC1WyrLZLRpXCcFYR1gQi0isWsZBTicC4efBOkkNG3r+1ue0gvtuU/tjREFGf4Y7HaKHGb5tNCOlMNeNjM5YLRwLFqrUSsQyD4rj4eua1ltearr24R0HilnTvnQm6Z/UY0s21vdOUFQBPY0GNAa+0wIDAQAB";
         String privateKey = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAIs1uYqX3+AufsF5//G1SSyELVbKstktGlcJwVhHWBCLSKxaxkFOJwLh58E6SQ0bev7W57SC+25T+2NEQUZ/hjsdoocZvm00I6Uw142MzlgtHAsWqtRKxDIPiuPh65rWW15quvbhHQeKWdO+dCbpn9RjSzbW905QVAE9jQY0Br7TAgMBAAECgYBcYhbzpr5no/Nyqmf0G/6nkEAWbQYrogbs5AhvcUk8EXL1DnirNhYlj42hafC4xhflrvCtlo8NNKaLxewbwN1uuzG8A2jd+ROEXlx5HDh2ZluhtHzL/SmNcJXo684xAl2pCNVBjDcW48PcIBijke/sTVHTDsDCukLKDPUOM/mKIQJBAL96k4+jBscazsJiuZ6C3RFDVtRRDpf1dMgLgxcx63bAXkA2Arau0J49IAYmSVJoDXqDoJKWdXJVh9vHSkhN/48CQQC6Hk1/G0Y0nOylf6NOp0oMgc0A+etnwxHKqwtctPKjEYcJx2fzALzTtCoySLYXX7gLnPIQXpQBTUysG5skBKp9AkEAiSQm6fqu0Q4fRlRlc+VwpnufhgPkOuw/z0OHiaZkajJPjxfgC63bl2paNG1ZmJ8UAEqkSDlhNxmRa9UqG+1ZewJASaQxz6gwCCNLM1SkfjuM/hPh1JAOh9jUUleJQF5MXx9RSho/VBQnorB3vbutaOQzw0yPLtDtSPKX8sVdhkveVQJAIDsJP5X8Tey6zXTUISor7PF0TSiKdE4k0IwKoy9y8HmQ+AU8+xyr/iOt5lvaGxKlBK8N/7yCw5H4qHnJaHT+Bg==";
