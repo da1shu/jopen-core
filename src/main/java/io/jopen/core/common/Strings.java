634bec5e8fa85219e9b3fba35280d979b61fae89
ubuntu-m <m17793873123@163.com>
2019-04-25 15:35:41 +0800

    update json

--- src/main/java/io/jopen/core/common/Strings.java
+++ src/main/java/io/jopen/core/common/Strings.java
@@ -191,4 +191,19 @@ public class Strings {
         number = "01.107700";
         System.out.println(number + " " + toChinese(number));
     }
+
+    //首字母转小写
+    public static String toLowerCaseFirstOne(String s){
+        if(Character.isLowerCase(s.charAt(0)))
+            return s;
+        else
+            return (new StringBuilder()).append(Character.toLowerCase(s.charAt(0))).append(s.substring(1)).toString();
+    }
+    //首字母转大写
+    public static String toUpperCaseFirstOne(String s){
+        if(Character.isUpperCase(s.charAt(0)))
+            return s;
+        else
+            return (new StringBuilder()).append(Character.toUpperCase(s.charAt(0))).append(s.substring(1)).toString();
+    }
 }
