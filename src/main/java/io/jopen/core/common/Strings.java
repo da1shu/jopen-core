30c25bd4271805565380b94ab24241a7a8fd6158
ubuntu-m <m17793873123@163.com>
2019-04-25 16:44:40 +0800

    update json

--- src/main/java/io/jopen/core/common/Strings.java
+++ src/main/java/io/jopen/core/common/Strings.java
@@ -197,13 +197,13 @@ public class Strings {
         if(Character.isLowerCase(s.charAt(0)))
             return s;
         else
-            return (new StringBuilder()).append(Character.toLowerCase(s.charAt(0))).append(s.substring(1)).toString();
+            return Character.toLowerCase(s.charAt(0)) + s.substring(1);
     }
     //首字母转大写
     public static String toUpperCaseFirstOne(String s){
         if(Character.isUpperCase(s.charAt(0)))
             return s;
         else
-            return (new StringBuilder()).append(Character.toUpperCase(s.charAt(0))).append(s.substring(1)).toString();
+            return Character.toUpperCase(s.charAt(0)) + s.substring(1);
     }
 }
