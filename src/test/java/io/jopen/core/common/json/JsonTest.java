0c42a94fb00ac3215efa0022966021b02ec083ab
ubuntu-m <m17793873123@163.com>
2019-06-03 12:19:02 +0800

    update StringHelper

--- src/test/java/io/jopen/core/common/json/JsonTest.java
+++ src/test/java/io/jopen/core/common/json/JsonTest.java
@@ -1,9 +1,11 @@
 package io.jopen.core.common.json;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * @author maxuefeng
@@ -42,5 +44,18 @@ public class JsonTest {
        /* // BUG
         JSONObject jsonObject = new JSONObject(new Student(LocalDateTime.now()));
         System.err.println(jsonObject);*/
+    }
+
+    @Test
+    public void testJSONArrayToJavaList(){
+        String source = "{\"ossKey\":\"583889989516001280\",\"data\":[\"饺子店\",\"5天前消费\"],\"accessUrl\":\"http://biz-qmbx.oss-cn-beijing.aliyuncs.com/583889989516001280\",\"ossBucket\":\"biz-qmbx\",\"formatName\":\"jpg\",\"OCRMode\":\"Tess\",\"ocrDataMode\":\"Single_text\",\"type\":1,\"insertTime\":1559280623044,\"uid\":\"582505345674469376\",\"identification\":-46766217,\"size\":68.1240234375,\"originCreateTime\":1559280620000,\"device\":\"ios\",\"originName\":\"1.jpg\"}";
+
+        JSONObject jsonObject = JSONObject.parseObject(source);
+
+        JSONArray jsonArray = jsonObject.getJSONArray("data");
+
+        List<String> list = jsonArray.toJavaList(String.class);
+
+        System.err.println(list);
     }
 }
